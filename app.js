1a82b6509b83a1a639b2de05e3f84c8bb1262977
Abdallah Ragab <a.ragab8882@gmail.com>
2023-01-04 15:58:51 +0200

    add Attack Methods

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
+
+function getRandomValue (min,max){
+    const attackValue = Math.floor(Math.random() * (max-min)) +min ;
+}
+
 const app = Vue.createApp({
     data () {
     return {
-
-    }
+        playerHealth : 100 ,
+        monsterHealth : 100,
+    };
     } ,
+    methods : {
+        attackMonster (){
+            // generate Random num between 5,12
+            const attackValue = getRandomValue(5,12);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        } ,
+        attackPlayer (){ 
+            // monster attack player 
+            const attackValue = getRandomValue(8,15);
+            this.playerHealth-=attackValue;
+        }
+    }
+
 });
 app.mount('#game');
