688e70a2084e1728b940428cb5644546647b7898
Abdallah Ragab <a.ragab8882@gmail.com>
2023-01-06 19:36:14 +0200

    handle error

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ const app = Vue.createApp({
       this.playerHealth = 100;
       this.monsterHealth = 100;
       this.currentRound = 0;
-      this.winner = "null";
+      this.winner = null;
       this.logMessages = [];
     },
     attackMonster() {
